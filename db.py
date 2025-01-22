f6745eafdaee8231838625d45e6a459d4fae21e7
ADSEO <ghassengharbi191@gmail.com>
2025-01-22 14:25:10 +0100

    db.py

--- db.py
+++ db.py
@@ -1,26 +1,48 @@
 import streamlit as st
-import pandas as pd
 import pymongo
 
-# Connexion à MongoDB
 client = pymongo.MongoClient("mongodb+srv://gas1991:gqsYfsbyCHfv2WjX@cluster0.9y1kj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client["Janvier"]
-collection = db["Tendances"]
 
-# Chargement des données depuis MongoDB
-df = pd.DataFrame(list(collection.find()))
-df = df.drop(columns=["_id"], errors="ignore")  # Supprime la colonne `_id`
+headerSection = st.container()
+mainSection = st.container()
+LeftNav =st.sidebar
+loginSection = st.container()
+logOutSection= st.container()
 
-# Trier les données et sélectionner les top résultats
-top_df = df.sort_values(by="Mots clŽs sur Google", ascending=False).head(100)  # Top 10 résultats
 
-# Afficher le graphique
-col_1,col_2 = st.columns(2)
-with col_1:
-    st.subheader("Tendance janvier 2024")
-    st.dataframe(df)
-    st.divider()
-with col_2:
-    st.subheader("Top 100")
-    st.divider()
-    st.bar_chart(top_df.set_index("Mots clŽs sur Google")["Visites"])
+
+def login():
+    with headerSection:
+        col_1,col_2=st.columns([2,1])
+    
+    with col_1:
+        st.title("Page de connexion")
+        
+    with col_2:
+        st.image("mytek.webp", width=200,)
+
+    st.markdown("<h2 style='font-size: 25px; color: #333;'>Se connecter avec son compte</h2>", unsafe_allow_html=True)
+    username = st.text_input("Identifiant")
+    password = st.text_input("Mot de passe",type="password")
+    if st.button("Se connecter"):
+        if username == "admin" and password =="password":
+            st.session_state.logged_in = True
+            st.success("Login successful")
+        else:
+            st.error("invalid username or password")
+
+def dashboard():
+    with headerSection:
+        st.title("Tendance Janvier 2025")        
+    with LeftNav:
+        st.text("hi")
+
+    if st.button("sign out"):
+        st.session_state.logged_in=False
+        st.experimental_rerun()
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in=False
+if st.session_state.logged_in:
+    dashboard()
+else:
+    login()
